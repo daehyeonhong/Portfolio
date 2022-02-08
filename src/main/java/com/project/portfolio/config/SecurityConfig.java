bab0606bad5676afa81ebf4eb4facd5a0e981b85
DaeHyeon <fholng@outlook.com>
2022-02-09 01:05:31 +0900

    [DB] Portfolio Project Database Init 개발 환경을 위한 H2 데이터 베이스 설정

--- src/main/java/com/project/portfolio/config/SecurityConfig.java
+++ src/main/java/com/project/portfolio/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.project.portfolio.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -11,5 +12,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/**", "/main", "/swagger-ui/**", "/swagger-ui").permitAll();
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/h2-console/**");
     }
 }
